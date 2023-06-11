de626645a6aa2dfed59600a2ef9dea76e48a1c59
Shruti <chatterjeeshruti500@gmail.com>
2023-06-11 14:41:44 +0530

    Initial commit

---
+++ js/code.js
@@ -0,0 +1,11 @@
+
+function nav() {
+    var x=document.getElementById("rightnav");
+    if(x.style.display=="none")
+    {
+        x.style.display="inline";
+    }
+    else{
+        x.style.display="none";
+    }
+}
